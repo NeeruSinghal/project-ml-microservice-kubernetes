2232b36428e1d3f817eadfaa1bb90e437894374c
NeeruSinghal <69108906+NeeruSinghal@users.noreply.github.com>
2020-09-07 15:52:30 +0530

    changes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -9,7 +9,7 @@ dockerpath=neerusinghal/api
 # Step 2
 # Run the Docker Hub container with kubernetes
 kubectl create deployment api --image=$dockerpath
-name='kubectl get pods --no-headers -o custom-columns=":metadata.name"'
+name=`kubectl get pods --no-headers -o custom-columns=":metadata.name"`
 
 # Step 3:
 # List kubernetes pods
